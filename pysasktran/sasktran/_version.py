1b1357284b047ee9c68aa2fbfca1d20b4113c51a
Daniel Zawada <daniel.zawada@usask.ca>
2023-07-07 18:32:33 -0600

    Try to update for new release

--- pysasktran/sasktran/_version.py
+++ pysasktran/sasktran/_version.py
@@ -41,7 +41,7 @@ def get_config():
     cfg = VersioneerConfig()
     cfg.VCS = "git"
     cfg.style = "pep440"
-    cfg.tag_prefix = "sasktran_"
+    cfg.tag_prefix = "v"
     cfg.parentdir_prefix = "sasktran-"
     cfg.versionfile_source = "sasktran/_version.py"
     cfg.verbose = False
