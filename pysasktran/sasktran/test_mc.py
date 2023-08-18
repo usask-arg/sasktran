8b5bf73336a751b9e3af8d3d48d28306ae52a1f6
Daniel Zawada <daniel.zawada@usask.ca>
2023-08-18 14:11:39 -0600

    Fix pycodestyle

--- pysasktran/sasktran/test_mc.py
+++ pysasktran/sasktran/test_mc.py
@@ -116,7 +116,7 @@ class TestEngineMC(unittest.TestCase):
         engine_output = engine.calculate_radiance()
         hardcodeMC = np.array([
             [6.40424550e-05, 9.00499796e-05, 7.17287790e-05, 6.05102733e-05],
-             [-3.56414296e-05, -9.05337267e-05, -3.17113375e-05, -5.91545520e-05]
+            [-3.56414296e-05, -9.05337267e-05, -3.17113375e-05, -5.91545520e-05]
         ])
         diff = np.abs((engine_output.ring_spectrum - hardcodeMC) / hardcodeMC)
         maxdiff = np.max(diff)
