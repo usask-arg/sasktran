36edd2e567c6c06b179b8fe3fa659ad64a8fb9ac
Daniel Zawada <daniel.zawada@usask.ca>
2023-12-13 11:28:29 -0600

    update

--- buildscripts/build_whl_sasktran_win.bat
+++ buildscripts/build_whl_sasktran_win.bat
@@ -7,6 +7,7 @@ ECHO %3
 
 call conda activate %PYENV%
 pushd pysasktran 
+python -m pip install setuptools-scm
 python -m pip wheel . -w ..\wheelhouse --no-deps --no-build-isolation
 IF %ERRORLEVEL% NEQ 0 EXIT /B 1
 popd
