45de5535c85e063195960722b1709ab326e97e43
Daniel Zawada <daniel.zawada@usask.ca>
2024-07-09 12:14:44 -0600

    scipyfix2

--- pysasktran/sasktran/legendre/__init__.py
+++ pysasktran/sasktran/legendre/__init__.py
@@ -128,15 +128,15 @@ def compute_greek_coefficients(p11: np.array, p12: np.array, p22: np.array, p33:
         lpoly_2m2 = wigner2m2.d(angle_rad, i)
         lpoly_02 = wigner02.d(angle_rad, i)
 
-        lm_a1[i] = simpson(p11 * lpoly_00, cos_theta) / simpson(lpoly_00 * lpoly_00, x=cos_theta)
-        lm_a4[i] = simpson(p44 * lpoly_00, cos_theta) / simpson(lpoly_00 * lpoly_00, x=cos_theta)
+        lm_a1[i] = simpson(p11 * lpoly_00, x=cos_theta) / simpson(lpoly_00 * lpoly_00, x=cos_theta)
+        lm_a4[i] = simpson(p44 * lpoly_00, x=cos_theta) / simpson(lpoly_00 * lpoly_00, x=cos_theta)
 
         if i >= 2:
-            lm_b1[i] = simpson(p12 * lpoly_02, cos_theta) / simpson(lpoly_02 * lpoly_02, x=cos_theta)
-            lm_b2[i] = -simpson(p34 * lpoly_02, cos_theta) / simpson(lpoly_02 * lpoly_02, x=cos_theta)
+            lm_b1[i] = simpson(p12 * lpoly_02, x=cos_theta) / simpson(lpoly_02 * lpoly_02, x=cos_theta)
+            lm_b2[i] = -simpson(p34 * lpoly_02, x=cos_theta) / simpson(lpoly_02 * lpoly_02, x=cos_theta)
 
-            temp1 = simpson((p22 + p33) * lpoly_22, cos_theta) / simpson(lpoly_22 * lpoly_22, x=cos_theta)
-            temp2 = simpson((p22 - p33) * lpoly_2m2, cos_theta) / simpson(lpoly_2m2 * lpoly_2m2, x=cos_theta)
+            temp1 = simpson((p22 + p33) * lpoly_22, x=cos_theta) / simpson(lpoly_22 * lpoly_22, x=cos_theta)
+            temp2 = simpson((p22 - p33) * lpoly_2m2, x=cos_theta) / simpson(lpoly_2m2 * lpoly_2m2, x=cos_theta)
 
             lm_a2[i] = (temp1 + temp2) / 2
             lm_a3[i] = (temp1 - temp2) / 2
